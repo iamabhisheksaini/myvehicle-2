1c0c8b7d7ca26952e28e69ec88075d49e911fa0f
Abhishek Saini <124495434+iamabhisheksaini@users.noreply.github.com>
2024-03-31 08:16:47 +0530

    Update utils.ts

--- src/Public/utils.ts
+++ src/Public/utils.ts
@@ -1,28 +1,23 @@
-// src/utils.ts
 import { Vehicle } from '../Components/types';
 
-
-
-
 export const mapJsonToVehicle = (jsonData: any): Vehicle => {
-  // Map JSON data to the Vehicle interface
   const { Name, Model, Manufacturer, "Manufacturing Date": manufacturingDate, Seating } = jsonData;
   const manufacturingYear = new Date(manufacturingDate).getFullYear();
 
-  // Generate a unique ID for each vehicle
-  const generateUniqueId = (): number => {
+  const generateUniqueId = (): string => {
     // Replace with your implementation to generate a unique number ID
-    return 1; 
+    return '1'; // Convert the number to a string
   };
 
-  // Create a Vehicle object
   const vehicle: Vehicle = {
-    id: generateUniqueId(), // Call generateUniqueId function to get unique ID
-    make: Manufacturer,
-    model: Model,
-    year: manufacturingYear,
-    // Add other properties as needed
+    id: generateUniqueId(),
+    Name,
+    Model,
+    Type: '', // Assuming you have a default value for Type
+    Manufacturer,
+    Seating,
   };
 
   return vehicle;
 };
+
